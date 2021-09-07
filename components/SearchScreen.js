83c343023cc3b91b4c8d5044806658516c78bc07
Atendimento2005 <gogo.june2005@gmail.com>
2021-09-08 00:02:20 +0530

    error fix

--- components/SearchScreen.js
+++ components/SearchScreen.js
@@ -13,7 +13,18 @@ import {
 } from 'react-native';
 import categoriesData from '../assets/data/categoriesData';
 import colors from '../assets/theme/colors';
+import searchCategoriesData from '../assets/data/searchCategoriesData';
 import productsData from '../assets/data/productsData';
+
+const renderSearchCategory = ({item}) => {
+  return (
+    <TouchableOpacity >
+        <View style={styles.searchCategoryWrapper}>
+          <Text style={styles.searchCategoryText}>{item.title}</Text>
+        </View>
+    </TouchableOpacity>
+  );
+}
 
 export default Home = () => {  
   return (
@@ -25,13 +36,31 @@ export default Home = () => {
           {/* Header */}
           <View style={styles.headerWrapper}>
             <View style = {styles.searchWrapper}>
-              <Image source={require('../assets/images/heroicons-outline_menu.png')}></Image>
+              <Image source={require('../assets/images/heroicons-outline_menu.png')} style = {{width: 29}}></Image>
               <View style = {styles.searchBarWrapper}>
                 <Text style = {styles.searchPlaceHolderText}>Type stuff here!</Text>
                 <Image source={require('../assets/images/carbon_search.png')} style = {{width: 24}}></Image>
               </View>
-              {/* <Image source={require()}></Image> */}
+              <Image source={require('../assets/images/el_shopping-cart.png')} style = {{width: 29}}></Image>
             </View>
+          </View>
+          {/* <View style={styles.searchSection}>
+            <Text style={styles.searchTitles}>Your top categories</Text>
+            <View>
+
+            </View>
+          </View>
+          <View style={styles.searchSection}>
+            <Text style={styles.searchTitles}>Your top categories</Text>
+          </View> */}
+          <View>
+            <FlatList
+              data={searchCategoriesData}
+              renderItem={renderSearchCategory}
+              keyExtractor={item => item.id}
+              horizontal = {true}
+              showsHorizontalScrollIndicator={false}
+            />
           </View>
         </ScrollView>
       </SafeAreaView>
@@ -47,13 +76,14 @@ const styles = StyleSheet.create({
   },
   headerWrapper: {
     backgroundColor: colors.white,
-    height: 112,
+    height: 90,
+    marginBottom: 10
   },
   searchWrapper : {
-    top: 47,
+    top: 20,
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-around',
+    justifyContent: 'space-evenly',
   },
   searchBarWrapper : {
     justifyContent: 'center',
@@ -70,15 +100,43 @@ const styles = StyleSheet.create({
     shadowRadius: 4.65,
     elevation: 5,
     height: 46,
-    width: 257,
+    width: 280,
   },
   searchPlaceHolderText: {
     color: colors.black,
-    width: 189, 
+    width: 220, 
     height: 19,  
     fontFamily: "Montserrat-Regular", 
     fontWeight: "500", 
     fontSize: 14, 
+  },
+  searchSection: {
+    // backgroundColor: colors.red,
+    height: 333,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    alignItems: 'flex-start'
+  },
+  searchTitles : {
+    "fontFamily": "Montserrat-Regular",
+    "fontWeight": "600",
+    "fontSize": 24,
+    margin: 5,
+  },
+  searchCategoryWrapper : {
+    width: 113.07,
+    height: 37,
+    backgroundColor: colors.white,
+    borderRadius: 25,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  searchCategoryText: {
+    "fontFamily": "Montserrat-Regular",
+    "fontWeight": "600",
+    "fontSize": 13,
+    margin: 5,
   }
 });
 
